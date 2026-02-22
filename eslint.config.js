ea9591dc5c60c0947b193bbfe1f69c74146e5144
daviads30 <davi.c.f.costa@gmail.com>
2026-02-22 04:14:48 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -18,12 +18,9 @@ export default tseslint.config(
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-empty-object-type": "off",
     },
-  },
+  }
 );
